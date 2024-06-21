1cdc4d0ac6fb69377fab1605b88cf19493fb1cc6
Keone Somers <Keone.somers@outlook.com>
2024-06-21 14:51:58 +0100

    blocked out basic layout

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
+  <div class="flex flex-col h-screen">
     <!-- <NuxtRouteAnnouncer /> -->
     <!-- <NuxtWelcome /> -->
+    <div class="h-14 flex border-b dark:border-zinc-600 px-4 items-center">
+      <span class="font-mono font-bold text-purple-400">keo.dev</span>
+    </div>
+    <div class="flex flex-1">
+      <div class="w-60 border-r dark:border-zinc-600 p-4">Side Bar</div>
+      <div class="flex flex-1">
+        <div class="flex-1 border-r dark:border-zinc-600 p-4">Notes</div>
+        <div class="flex-1 p-4">Chat</div>
+      </div>
+    </div>
   </div>
 </template>
