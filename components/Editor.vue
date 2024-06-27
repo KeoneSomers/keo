3b3658aa05545b7d01a9a849329975317fce630b
Keone Somers <Keone.somers@outlook.com>
2024-06-27 21:00:00 +0100

    editor commands now highlight if active

--- components/Editor.vue
+++ components/Editor.vue
@@ -110,7 +110,7 @@ const saveNotes = async () => {
                 @click="editor.chain().focus().toggleBold().run()"
                 :disabled="!editor.can().chain().focus().toggleBold().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('bold') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('bold') }"
             >
                 <RiBold size="16px"/>
             </button>
@@ -118,7 +118,7 @@ const saveNotes = async () => {
                 @click="editor.chain().focus().toggleItalic().run()"
                 :disabled="!editor.can().chain().focus().toggleItalic().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('italic') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('italic') }"
             >
                 <RiItalic size="16px"/>
             </button>
@@ -126,7 +126,7 @@ const saveNotes = async () => {
                 @click="editor.chain().focus().toggleStrike().run()"
                 :disabled="!editor.can().chain().focus().toggleStrike().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('strike') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('strike') }"
             >
                 <RiStrikethrough size="16px"/>
             </button>
@@ -134,7 +134,7 @@ const saveNotes = async () => {
                 @click="editor.chain().focus().toggleCode().run()"
                 :disabled="!editor.can().chain().focus().toggleCode().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('code') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('code') }"
             >
                 <RiCodeLine size="16px"/>
             </button>
@@ -153,77 +153,77 @@ const saveNotes = async () => {
             <button
                 @click="editor.chain().focus().setParagraph().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('paragraph') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('paragraph') }"
             >
                 <RiParagraph size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 1 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 1 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 1 }) }"
             >
                 <RiH1 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 2 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 2 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 2 }) }"
             >
                 <RiH2 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 3 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 3 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 3 }) }"
             >
                 <RiH3 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 4 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 4 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 4 }) }"
             >
                 <RiH4 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 5 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 5 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 5 }) }"
             >
                 <RiH5 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleHeading({ level: 6 }).run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('heading', { level: 6 }) }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('heading', { level: 6 }) }"
             >
                 <RiH6 size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleBulletList().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('bulletList') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('bulletList') }"
             >
                 <RiListUnordered size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleOrderedList().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('orderedList') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('orderedList') }"
             >
                 <RiListOrdered size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleCodeBlock().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('codeBlock') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('codeBlock') }"
             >
                 <RiCodeBlock size="16px"/>
             </button>
             <button
                 @click="editor.chain().focus().toggleBlockquote().run()"
                 class="m-1 bg-zinc-100 dark:bg-zinc-800 rounded p-1 text-xs"
-                :class="{ 'is-active': editor.isActive('blockquote') }"
+                :class="{ 'bg-zinc-900 text-white dark:bg-zinc-100 dark:text-zinc-900': editor.isActive('blockquote') }"
             >
                 <RiQuoteText size="16px"/>
             </button>
