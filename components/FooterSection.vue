7eba008e695e5e394a99c6e89bd10737536e2c49
Keone Somers <Keone.somers@outlook.com>
2024-06-27 16:04:39 +0100

    added light mode to landing page

--- components/FooterSection.vue
+++ components/FooterSection.vue
@@ -74,26 +74,26 @@
 </script>
 
 <template>
-  <footer class="bg-zinc-900 border-t border-zinc-700">
-    <div
-      class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"
-    >
-      <!-- <div class="flex justify-center space-x-6 md:order-2">
-        <a
-          v-for="item in navigation"
-          :key="item.name"
-          :href="item.href"
-          class="text-gray-400 hover:text-gray-500"
+    <footer class="dark:bg-zinc-900 border-t dark:border-zinc-700">
+        <div
+            class="mx-auto max-w-7xl px-6 py-12 md:flex md:items-center md:justify-between lg:px-8"
         >
-          <span class="sr-only">{{ item.name }}</span>
-          <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
-        </a>
-      </div> -->
-      <div class="mt-8 md:order-1 md:mt-0">
-        <p class="text-center text-xs leading-5 text-gray-500">
-          &copy; 2024 Keo.dev. All rights reserved.
-        </p>
-      </div>
-    </div>
-  </footer>
+            <!-- <div class="flex justify-center space-x-6 md:order-2">
+              <a
+                v-for="item in navigation"
+                :key="item.name"
+                :href="item.href"
+                class="text-gray-400 hover:text-gray-500"
+              >
+                <span class="sr-only">{{ item.name }}</span>
+                <component :is="item.icon" class="h-6 w-6" aria-hidden="true" />
+              </a>
+            </div> -->
+            <div class="mt-8 md:order-1 md:mt-0">
+                <p class="text-center text-xs leading-5 text-gray-500">
+                    &copy; 2024 Keo.dev. All rights reserved.
+                </p>
+            </div>
+        </div>
+    </footer>
 </template>
